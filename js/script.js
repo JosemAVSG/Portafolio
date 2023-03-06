2d07800782ec933ac7d252be1fcb42f714f3b2b4
Jose Miguel <jmgg.95n@gmail.com>
2023-03-06 18:24:20 -0500

    creating portaforlio all

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+   $('.customer-logos').slick({
+    slidesToShow: 6,
+    slidesToScroll:1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows:false,
+    dots:false,
+    pauseOnHover:false,
+    responsive:[{
+        breakpoint:768,
+        settings:{
+            slidesToShow:4
+        }
+
+    },{ 
+        breakpoint:520,
+        settings:{
+            slidesToShow:3
+        }
+      
+    }]
+
+   });
+});
+/*
+$(document).ready(function(){
+    $('.partners-logo').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover:false,
+        responsive: [{
+            breakpoint: 768,
+            setting: {
+                slidesToShow:4
+            }
+        }, {
+            breakpoint: 520,
+            setting: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});*/
